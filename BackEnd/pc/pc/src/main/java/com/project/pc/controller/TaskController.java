a668999cfe788d03dfe1675832800d0f88373b02
danut <danutvaleriu.termure@gmail.com>
2023-05-11 00:18:17 +0300

    Added GET methods to TaskController and TaskService.

--- BackEnd/pc/pc/src/main/java/com/project/pc/controller/TaskController.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/controller/TaskController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin("http://localhost:8080")
 @RestController
 @RequestMapping("/api/tasks")
@@ -18,5 +20,17 @@ public class TaskController {
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task){
         return new ResponseEntity<>(taskService.createTask(task), HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks(){
+        return new ResponseEntity<>(taskService.getAllTasks(), HttpStatus.OK);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable("id") Long id){
+        Task task = taskService.getTaskById(id);
+        if (task == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(task, HttpStatus.OK);
     }
 }
--- BackEnd/pc/pc/src/main/java/com/project/pc/service/TaskService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/TaskService.java
@@ -5,11 +5,20 @@ import com.project.pc.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
     public Task createTask(Task task){
         return taskRepository.save(new Task(task.getGrade(), task.getDescription(), task.getDeadline(), task.getAttendance()));
+    }
+    public List<Task> getAllTasks(){
+        List<Task> tasks = taskRepository.findAll();
+        return tasks;
+    }
+    public Task getTaskById(Long id){
+        return taskRepository.findById(id).orElse(null);
     }
 }
