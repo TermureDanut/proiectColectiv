c197cb2876519a06818a2b593e182fe2a22ffc3d
danut <danutvaleriu.termure@gmail.com>
2023-05-10 23:42:20 +0300

    Added GET methods to MentorController and MentorService.

--- BackEnd/pc/pc/src/main/java/com/project/pc/controller/MentorController.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/controller/MentorController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @CrossOrigin("http://localhost:8080")
 @RestController
 @RequestMapping("/api/mentors")
@@ -17,5 +19,18 @@ public class MentorController {
     public ResponseEntity<Mentor> createMentor(@RequestBody Mentor mentor){
         Mentor createdMentor = mentorService.createMentor(mentor);
         return new ResponseEntity<>(createdMentor, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<Mentor>> getAllMentors(){
+        List<Mentor> mentors = mentorService.getAllMentors();
+        return new ResponseEntity<>(mentors, HttpStatus.OK);
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<Mentor> getMentorById(@PathVariable("id") Long id){
+        Mentor mentor = mentorService.getMentorById(id);
+        if (mentor == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(mentor, HttpStatus.OK);
     }
 }
--- BackEnd/pc/pc/src/main/java/com/project/pc/service/MentorService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/MentorService.java
@@ -5,11 +5,21 @@ import com.project.pc.repository.MentorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MentorService {
     @Autowired
     private MentorRepository mentorRepository;
     public Mentor createMentor(Mentor mentor){
         return mentorRepository.save(new Mentor(mentor.getName(), mentor.getEmail()));
+    }
+    public List<Mentor> getAllMentors(){
+        List<Mentor> mentors = mentorRepository.findAll();
+        return mentors;
+    }
+    public Mentor getMentorById(Long id){
+        return mentorRepository.findMentorById(id).orElse(null);
     }
 }
