edd3e4c24ca9693b758d391487f78363d2f38667
danut <danutvaleriu.termure@gmail.com>
2023-05-10 20:54:46 +0300

    Added GET methods to ActivityController and ActivityService.

--- BackEnd/pc/pc/src/main/java/com/project/pc/controller/ActivityController.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/controller/ActivityController.java
@@ -1,12 +1,15 @@
 package com.project.pc.controller;
 
 import com.project.pc.model.Activity;
+import com.project.pc.model.Student;
 import com.project.pc.repository.ActivityRepository;
 import com.project.pc.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
@@ -18,5 +21,18 @@ public class ActivityController {
     public ResponseEntity<Activity> createActivity(@RequestBody Activity activity) {
         Activity createdActivity = activityService.createActivity(activity);
         return new ResponseEntity<>(createdActivity, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<Activity>> getAllActivities(){
+        List<Activity> activities = activityService.getAllActivities();
+        return new ResponseEntity<>(activities, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Activity> getActivityById(@PathVariable("id") Long id) {
+        Activity activity = activityService.getActivityById(id);
+        if (activity == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(activity, HttpStatus.FOUND);
     }
 }
--- BackEnd/pc/pc/src/main/java/com/project/pc/service/ActivityService.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/service/ActivityService.java
@@ -1,9 +1,13 @@
 package com.project.pc.service;
 
 import com.project.pc.model.Activity;
+import com.project.pc.model.Student;
 import com.project.pc.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ActivityService {
@@ -11,5 +15,17 @@ public class ActivityService {
     private ActivityRepository activityRepository;
     public Activity createActivity(Activity activity){
         return activityRepository.save(new Activity(activity.getName(), activity.getDescription()));
+    }
+    public List<Activity> getAllActivities(){
+        List<Activity> activities = new ArrayList<>();
+        activityRepository.findAll().forEach(activities::add);
+        return activities;
+    }
+    public Activity getActivityById (Long id) {
+        Activity activity = activityRepository.findById(id).orElse(null);
+        if (activity == null){
+            return null;
+        }
+        return activity;
     }
 }
