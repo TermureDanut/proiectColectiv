747d948c10dc84aa90f1b6af5262e1b7c3feea52
danut <danutvaleriu.termure@gmail.com>
2023-05-17 19:26:27 +0300

    Linked the Team model to the Activity model and to Mentor model.

--- BackEnd/pc/pc/src/main/java/com/project/pc/model/Team.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/model/Team.java
@@ -1,6 +1,10 @@
 package com.project.pc.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "teams")
@@ -10,6 +14,18 @@ public class Team {
     private long id;
     @Column
     private long teamLeader;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @Nullable
+    private Activity activity;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mentor_id")
+    @OnDelete(action = OnDeleteAction.NO_ACTION)
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @Nullable
+    private Mentor mentor;
     public Team() {}
     public Team(long teamLeader) {
         this.teamLeader = teamLeader;
