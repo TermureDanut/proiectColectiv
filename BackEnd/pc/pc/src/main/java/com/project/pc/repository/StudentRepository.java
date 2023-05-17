f394331e53229b41a8628e86a731504f719cdd65
danut <danutvaleriu.termure@gmail.com>
2023-05-17 19:34:22 +0300

    Added new method to StudentRepository.

--- BackEnd/pc/pc/src/main/java/com/project/pc/repository/StudentRepository.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/repository/StudentRepository.java
@@ -11,4 +11,5 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findStudentById(long id);
     List<Student> findStudentByName(String name);
     Optional<Student> findStudentByEmail(String email);
+    List<Student> findByTeamId(Long tID);
 }
