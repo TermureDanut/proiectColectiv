158f9c047c91b84673b86823896fbcea1189de41
danut <danutvaleriu.termure@gmail.com>
2023-06-02 16:32:01 +0300

    Modiffications to task model constructors and links to other models.

--- BackEnd/pc/pc/src/main/java/com/project/pc/model/Task.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/model/Task.java
@@ -1,11 +1,14 @@
 package com.project.pc.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.lang.NonNull;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table
@@ -27,59 +30,38 @@ public class Task {
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     @Nullable
     private Activity activity;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "student_id")
-    @OnDelete(action = OnDeleteAction.NO_ACTION)
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    @Nullable
-    private Student student;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "team_id")
-    @OnDelete(action = OnDeleteAction.NO_ACTION)
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    @Nullable
-    private Team team;
-    public Task(){}
-    public Task(int grade, String description, String deadline, int attendance) {
-        this.grade = grade;
+    @ManyToMany(fetch = FetchType.LAZY,  mappedBy = "tasks")
+    @JsonIgnore
+    private Set<Mentor> mentors = new HashSet<>();
+    @ManyToMany(fetch = FetchType.LAZY,  mappedBy = "tasks")
+    @JsonIgnore
+    private Set<Student> students = new HashSet<>();
+    public Task(){
+        this.grade = this.attendance = 0;
+        this.description = this.deadline = "";
+    }
+    public Task(String description, String deadline) {
+        this.grade = 0;
         this.description = description;
         this.deadline = deadline;
-        this.attendance = attendance;
+        this.attendance = 0;
     }
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public int getGrade() {
-        return grade;
-    }
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public String getDeadline() {
-        return deadline;
-    }
-    public void setDeadline(String deadline) {
-        this.deadline = deadline;
-    }
-    public int getAttendance() {
-        return attendance;
-    }
-    public void setAttendance(int attendance) {
-        this.attendance = attendance;
-    }
+    public long getId() {return id;}
+    public void setId(long id) {this.id = id;}
+    public int getGrade() {return grade;}
+    public void setGrade(int grade) {this.grade = grade;}
+    public String getDescription() {return description;}
+    public void setDescription(String description) {this.description = description;}
+    public String getDeadline() {return deadline;}
+    public void setDeadline(String deadline) {this.deadline = deadline;}
+    public int getAttendance() {return attendance;}
+    public void setAttendance(int attendance) {this.attendance = attendance;}
     public Activity getActivity() {return activity;}
     public void setActivity(Activity activity) {this.activity = activity;}
-    public Student getStudent() {return student;}
-    public void setStudent(Student student) {this.student = student;}
-    public Team getTeam() {return team;}
-    public void setTeam(Team team) {this.team = team;}
+    public Set<Mentor> getMentors() {return mentors;}
+    public void setMentors(Set<Mentor> mentors) {this.mentors = mentors;}
+    public void addMentorToMentors(Mentor mentor){this.mentors.add(mentor);}
+    public Set<Student> getStudents() {return students;}
+    public void setStudents(Set<Student> students) {this.students = students;}
+    public void addStudentToStudent(Student student) {this.students.add(student);}
 }
