89903de504d9abdfd09b21c2c889d0e303912536
danut <danutvaleriu.termure@gmail.com>
2023-06-07 13:34:26 +0300

    Modiffied ActivityDTO for name to be unique.

--- BackEnd/pc/pc/src/main/java/com/project/pc/dto/ActivityDTO.java
+++ BackEnd/pc/pc/src/main/java/com/project/pc/dto/ActivityDTO.java
@@ -1,9 +1,11 @@
 package com.project.pc.dto;
 
+import jakarta.persistence.Column;
 import lombok.Data;
 
 @Data
 public class ActivityDTO {
+    @Column(unique = true)
     private String name;
     private String description;
     private String deadline;
